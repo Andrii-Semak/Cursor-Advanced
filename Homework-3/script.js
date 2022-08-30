69dfd28dafc429f91746d4175b655230025c829e
Andrii Semak <83277879+Andrii-Semak@users.noreply.github.com>
2022-08-30 15:10:45 +0300

    Fixed functions

--- Homework-3/script.js
+++ Homework-3/script.js
@@ -1,12 +1,12 @@
 //1 function
 
 buttone.onclick = function() {
-let n = document.getElementById('firstinput').value;
-document.getElementById('resultone').innerHTML=` \u00A0 ${getMaxDigit(Number(n))}`;
+let maxdigit = document.getElementById('firstinput').value;
+document.getElementById('resultone').innerHTML=` \u00A0 ${getMaxDigit(Number(maxdigit))}`;
  
-        function getMaxDigit(num) {
-            let result = -1;
-            for (let i = num; i > 0; i = Math.floor(i / 10)) {
+        function getMaxDigit(number) {
+            let result = 0;
+            for (let i = number; i > 0; i = Math.floor(i / 10)) {
                 let digit = i % 10;
                 if (result < digit)
                     result = digit;
@@ -18,17 +18,17 @@ document.getElementById('resultone').innerHTML=` \u00A0 ${getMaxDigit(Number(n))
 //2 function
 
 butttwo.onclick = function() {
-    let a = document.getElementById('number').value;
-    let b = document.getElementById('degree').value;
-    let answer = a;
-        if (b == 0) {
+    let number = document.getElementById('number').value;
+    let degree = document.getElementById('degree').value;
+    let answer = number;
+        if (degree == 0) {
             document.getElementById('resulttwo').innerHTML=` \u00A0 1`;
-        } else if (b == 1) {
-            document.getElementById('resulttwo').innerHTML=` \u00A0 ${a}`;
+        } else if (degree == 1) {
+            document.getElementById('resulttwo').innerHTML=` \u00A0 ${number}`;
         }
-        while (b > 1) {
-            answer = answer * a;
-            b = b - 1;
+        while (degree > 1) {
+            answer = answer * number;
+            degree = degree - 1;
             document.getElementById('resulttwo').innerHTML=` \u00A0 ${answer}`;
         }                
 };
@@ -44,20 +44,20 @@ buttthree.onclick = function() {
 //4 function
 
 buttfour.onclick = function() {
-    let c = document.getElementById('salary').value;
-    let salary = c - c * 0.195;
+    let salarywithouttax = document.getElementById('salary').value;
+    let salary = salarywithouttax - salarywithouttax * 0.195;
     document.getElementById('resultfour').innerHTML=` \u00A0 ${salary}`;
 };
 
 //5 function
 
 buttfive.onclick = function() {
-    let a = document.getElementById('numberone').value;
-    let b = document.getElementById('numbertwo').value;
+    let numberone = document.getElementById('numberone').value;
+    let numbertwo = document.getElementById('numbertwo').value;
     function getRandom(min, max) {
         return Math.random() * (max - min) + min;
       }
-    let result = getRandom(Number(a), Number(b));
+    let result = getRandom(Number(numberone), Number(numbertwo));
     document.getElementById('resultfive').innerHTML=` \u00A0 ${Math.floor(result)}`;             
 };
 
@@ -66,9 +66,9 @@ buttfive.onclick = function() {
 buttsix.onclick = function() {
     let letter = document.getElementById('letter').value;
     let word = document.getElementById('word').value;
-    let letterr = letter.charAt(0).toLowerCase();
-    let wordd = word.charAt(0).toLowerCase() + word.slice(1).toLowerCase();
-    let count = [...wordd].filter(l => l === letterr).length;
+    let letterfixed = letter.charAt(0).toLowerCase();
+    let wordfixed = word.charAt(0).toLowerCase() + word.slice(1).toLowerCase();
+    let count = [...wordfixed].filter(l => l === letterfixed).length;
     document.getElementById('resultsix').innerHTML=` \u00A0 ${count}`;
 };
 
@@ -98,7 +98,8 @@ buttseven.onclick = function() {
 
 buttnine.onclick = function() {
     let size = document.getElementById('size').value;
-    const getRandomPassword = (size) => {
+    const getRandomPassword = (size = 8) => {
+        
         result = '';
         for (i = 0; i < size; i++) {
             result = result + (Math.floor(Math.random() * 10)).toString();
