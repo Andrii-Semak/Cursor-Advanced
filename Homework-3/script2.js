3debaa739b7ced3e3c92e61b778521efc0ed7f47
Andrii Semak <83277879+Andrii-Semak@users.noreply.github.com>
2022-08-30 15:11:46 +0300

    Homework-3 Fixed functions

--- Homework-3/script2.js
+++ Homework-3/script2.js
@@ -1,7 +1,7 @@
 //1 function
 
 const getMaxDigit = (num) => {
-    let result = -1;
+    let result = 0;
                 for (let i = num; i > 0; i = Math.floor(i / 10)) {
                     let digit = i % 10;
                     if (result < digit)
@@ -12,53 +12,53 @@ const getMaxDigit = (num) => {
 
 //2 function
 
-const Degree = (c, d) => {
-    let answer = c;
-        if (d == 0) {
+const getNumnerDegree = (number, degree) => {
+    let answer = number;
+        if (degree == 0) {
             document.writeln(` \u00A0 1`);
-        } else if (d == 1) {
-            document.writeln(` \u00A0 ${a}`);
+        } else if (degree == 1) {
+            document.writeln(` \u00A0 ${number}`);
         }
-        while (d > 1) {
-            answer = answer * c;
-            d = d - 1;
+        while (degree > 1) {
+            answer = answer * number;
+            degree = degree - 1;
         }
         return answer;   
 }
 
 //3 function
 
-const LetterBIG = (e) => {
-    let nameresult = e.charAt(0).toUpperCase() + e.slice(1).toLowerCase();
+const getFormatedName = (nametoformat) => {
+    let nameresult = nametoformat.charAt(0).toUpperCase() + nametoformat.slice(1).toLowerCase();
     return nameresult;
 }
 
 //4 function
 
-const SalaryFEE = (g) => {
-    let salary = g - g * 0.195;
+const getSalaryWithTax = (salarybefore) => {
+    let salary = salarybefore - salarybefore * 0.195;
     return salary;
 }
 
 //5 function
 
-const getRandom = (min, max) => {
+const getRandomNumber = (min, max) => {
     let resultrandom = Math.random() * (max - min) + min;
     return resultrandom;
 }
 
 //6 function
 
-const CountLetter = (ltr, w) => {
+const getLetterAmount = (ltr, word) => {
     let letterr = ltr.charAt(0).toLowerCase();
-    let wordd = w.charAt(0).toLowerCase() + w.slice(1).toLowerCase();
+    let wordd = word.charAt(0).toLowerCase() + word.slice(1).toLowerCase();
     let count = [...wordd].filter(l => l === letterr).length;
     return count;
 };
 
 //7 function
 
-const convert = (sum) => {
+const getConvertedMoney = (sum) => {
     let converted;
     sum = sum.toUpperCase();
     if (sum.indexOf('UAH') != -1) {
@@ -73,7 +73,7 @@ const convert = (sum) => {
 
 //9 function
 
-const getRandomPassword = (size) => {
+const getRandomPassword = (size = 8) => {
     resultpass = '';
     for (i = 0; i < size; i++) {
         resultpass = resultpass + (Math.floor(Math.random() * 10)).toString();
@@ -85,29 +85,29 @@ const getRandomPassword = (size) => {
 
 //Вивід усіх функцій
 
-let n = 5874;
-document.getElementById('all1').innerHTML=`Функція №1 <br> В числі: ${n} найбільшою цифрою є: ${getMaxDigit(n)} <br>`;
+let maxdigit = 5874;
+document.getElementById('all1').innerHTML=`Функція №1 <br> В числі: ${maxdigit} найбільшою цифрою є: ${getMaxDigit(maxdigit)} <br>`;
 
-let a = 5;
-let b = 3;
-document.getElementById('all2').innerHTML=`Функція №2 <br> Піднявши число ${a} до ${b} степені отримаємо : ${Degree(a, b)} <br>`;
+let number = 5;
+let degree = 3;
+document.getElementById('all2').innerHTML=`Функція №2 <br> Піднявши число ${number} до ${degree} степені отримаємо : ${getNumnerDegree(number, degree)} <br>`;
 
-let f = "воЛОдИмир";
-document.getElementById('all3').innerHTML=`Функція №3 <br> Після форматування імені: ${f}, отримаємо: ${LetterBIG(f)} <br>`;
+let nametoformat = "воЛОдИмир";
+document.getElementById('all3').innerHTML=`Функція №3 <br> Після форматування імені: ${nametoformat}, отримаємо: ${getFormatedName(nametoformat)} <br>`;
 
-let j = 15000;
-document.getElementById('all4').innerHTML=`Функція №4 <br> Якщо ваша зарплата: ${j} то після врахування податку отримаємо: ${SalaryFEE(j)} <br>`;
+let salary = 15000;
+document.getElementById('all4').innerHTML=`Функція №4 <br> Якщо ваша зарплата: ${salary} то після врахування податку отримаємо: ${getSalaryWithTax(salary)} <br>`;
 
 let minnumber = 10;
 let maxnumber = 100;
-document.getElementById('all5').innerHTML=`Функція №5 <br> В проміжку між : ${minnumber} та ${maxnumber} рандомно отримаємо: ${Math.floor(getRandom(minnumber, maxnumber))} <br>`;
+document.getElementById('all5').innerHTML=`Функція №5 <br> В проміжку між : ${minnumber} та ${maxnumber} рандомно отримаємо: ${Math.floor(getRandomNumber(minnumber, maxnumber))} <br>`;
 
 let letter = "а";
 let word = "Антиматерія";
-document.getElementById('all6').innerHTML=`Функція №6 <br> В слові: ${word}, кількість букви ${letter} = ${CountLetter(letter, word)} <br>`;
+document.getElementById('all6').innerHTML=`Функція №6 <br> В слові: ${word}, кількість букви ${letter} = ${getLetterAmount(letter, word)} <br>`;
 
 let money = "10000UAH";
-document.getElementById('all7').innerHTML=`Функція №7 <br> При конвертації ${money} в $ отримаємо: ${money} = ${convert(money)} <br>`;
+document.getElementById('all7').innerHTML=`Функція №7 <br> При конвертації ${money} в $ отримаємо: ${money} = ${getConvertedMoney(money)} <br>`;
 
 let sizeOfPass = 7;
 document.getElementById('all9').innerHTML=`Функція №9 <br> Користувач хоче пароль довжиною: ${sizeOfPass}, наша програма видасть: ${getRandomPassword(sizeOfPass)} <br>`;
